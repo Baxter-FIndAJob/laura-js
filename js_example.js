7ddc77e0792d67b0dd3504c1c3ce75a1d895df6b
Laura Morris <laura.morris@baxter-academy.org>
2018-04-13 10:45:19 -0400

    Progress

--- js_example.js
+++ js_example.js
@@ -1,12 +1,22 @@
 var onButtonClick = function(){
 	console.log('#wasclicked was clicked')
-		window.alert("#wasclicked");
+	location.reload();
 }
 
+var onMouseEnter = function(){
+	console.log("The mouse entered the element")
+}
+
+var onMouseLeaves = function(){
+	console.log("The mouse has left")
+}
 
 var mainProgram = function(){
-	$('#wasclicked').click(onButtonClick);
+	$('#clickable').click(onButtonClick);
+	$('#enterable').mouseenter(onMouseEnter)
+	$('#leaveable').mouseleave(onMouseLeaves)
 }
 
 $(mainProgram);
 
+//alert("#wasclicked");
